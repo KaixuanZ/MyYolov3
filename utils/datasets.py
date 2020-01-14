848a1297f1bc9956cd3501a54806262459489968
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2020-01-14 12:33:42 -0500

    remove unnecessary code

--- utils/datasets.py
+++ utils/datasets.py
@@ -11,20 +11,8 @@ import pandas as pd
 from utils.augmentations import *
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
+from torch.autograd import Variable
 
-'''
-def pad_to_square(img, pad_value):
-    c, h, w = img.shape
-    dim_diff = np.abs(h - w)
-    # (upper / left) padding and (lower / right) padding
-    pad1, pad2 = dim_diff // 2, dim_diff - dim_diff // 2
-    # Determine padding
-    pad = (0, 0, pad1, pad2) if h <= w else (pad1, pad2, 0, 0)
-    # Add padding
-    img = F.pad(img, pad, "constant", value=pad_value)
-
-    return img, pad
-'''
 
 def resize(image, size):
     img_size=[int(size[0]/32)*32,int(size[1]/32)*32]
@@ -111,19 +99,9 @@ class ListDataset(Dataset):
 
         # Extract image as PyTorch tensor
         img = transforms.ToTensor()(Image.open(img_path).convert('L'))
-        #print(img.shape)
         # Handle images with less than three channels
         if len(img.shape) != 3:	#only H,W, add another dimension C
             img = img.unsqueeze(0) # C=1
-            #img = img.expand((3, img.shape[1:]))
-
-        _, h, w = img.shape
-        h_factor, w_factor = (h, w) if self.normalized_labels else (1, 1)
-        # Pad to square resolution
-        #img, pad = pad_to_square(img, 0)
-        img = resize(img, self.img_size)
-        _, padded_h, padded_w = img.shape
-
         # ---------
         #  Label
         # ---------
@@ -133,41 +111,25 @@ class ListDataset(Dataset):
         targets = None
         if os.path.exists(label_path):
             boxes = torch.from_numpy(np.loadtxt(label_path).reshape(-1, 5)) #(x, y, w, h)
-            '''
-            # Extract coordinates for unpadded + unscaled image
-            x1 = w * (boxes[:, 1] - boxes[:, 3] / 2)
-            y1 = h * (boxes[:, 2] - boxes[:, 4] / 2)
-            x2 = w * (boxes[:, 1] + boxes[:, 3] / 2)
-            y2 = h * (boxes[:, 2] + boxes[:, 4] / 2)
-            
-            # Adjust for added padding
-            x1 += pad[0]
-            y1 += pad[2]
-            x2 += pad[1]
-            y2 += pad[3]
-            
-            # Returns (x, y, w, h)
-            boxes[:, 1] = ((x1 + x2) / 2) / padded_w
-            boxes[:, 2] = ((y1 + y2) / 2) / padded_h
-            boxes[:, 3] *= w_factor / padded_w
-            boxes[:, 4] *= h_factor / padded_h
-            '''
             targets = torch.zeros((len(boxes), 6))
             targets[:, 1:] = boxes
-
         # Apply augmentations
         if self.augment:
             # we can save data in this class but if the training data is too much we cannot remember them in memory
-            # add noise
-
-            img, targets = rotate(img, targets, 1)
             if np.random.random() < 0.5:
-                # random small affine transform
-                pass
+                # random small perspective transform
+                t = 0.2*(np.random.random()-0.5)
+                img, targets = perspective(img.permute(1, 2, 0).numpy(), targets, t)
             else:
                 # random rotation (small degree)
-                pass
-
+                theta = 10 * (np.random.random()-0.5)
+                img, targets = rotate(img.permute(1,2,0).numpy(), targets, theta)
+            # add noise
+            img = transforms.ToTensor()(img)
+            img += Variable(img.data.new(img.size()).normal_(0, 0.01))
+        img = resize(img, self.img_size)
+        #plot_bbox(img.permute(1, 2, 0).numpy(), targets, 'tmp')
+        #import pdb;pdb.set_trace()
         return img_path, img, targets
 
     def collate_fn(self, batch):
